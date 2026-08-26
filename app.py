9bb478b69e8285a44723e370cef806374278dffa
Gabe-Rose <roseg@carleton.edu>
2026-08-25 21:01:23 -0600

    ...

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def index_page():
     if current_term.get("error") is not None:
       abort(500)
 
-    term_in = current_term['id']
+    term_in = current_term.get('id')
     day_in = mn_time.strftime('%A')
     time_in = mn_time.strftime("%H:%M")
     show = query_db(term_in, day_in, time_in)
