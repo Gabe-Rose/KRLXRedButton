8876abfd3f34d99862753c9f3b8156614aaf1f77
Gabe-Rose <roseg@carleton.edu>
2026-08-25 21:12:53 -0600

    ...

--- app.py
+++ app.py
@@ -61,7 +61,14 @@ def query_db(term, day, current_time):
       shows.published_start <= @query_time AND
       shows.published_end > @query_time AND
       shows.published_start <= shows.published_end)
-      
+      OR
+      (shows.published_day = @query_day AND
+      shows.published_start <= @query_time AND
+      shows.published_start > shows.published_end)
+      OR
+      (shows.published_day = @query_prev_day AND
+      shows.published_end >= @query_time AND
+      shows.published_start > shows.published_end)
       )
       GROUP BY shows.id
       ORDER BY shows.published_start ASC
@@ -130,9 +137,9 @@ def index_page():
     if current_term.get("error") is not None:
       abort(500)
 
-    term_in = current_term.get('id')
-    day_in = mn_time.strftime('%A')
-    time_in = mn_time.strftime("%H:%M")
+    term_in = str(current_term.get('id'))
+    day_in = str(mn_time.strftime('%A'))
+    time_in = str(mn_time.strftime("%H:%M"))
     show = query_db(term_in, day_in, time_in)
     return show
     if show.get("error") is not None:
